cd5bf55100c01ce13ff79696158ff992fd041834
jeremie_giffard <e132784f@gmail.com>
2022-05-06 13:50:23 +0200

    Updates

---
+++ precache-manifest.d6a0f26d33df80c8d326c075817f5932.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3055d5451af88dd9621c3541d8fefa2f",
+    "url": "/ps-flux/index.html"
+  },
+  {
+    "revision": "a9a5a23a8af7f8332b0b",
+    "url": "/ps-flux/static/css/2.b62f5c36.chunk.css"
+  },
+  {
+    "revision": "a9a5a23a8af7f8332b0b",
+    "url": "/ps-flux/static/js/2.2b5c81f8.chunk.js"
+  },
+  {
+    "revision": "04ed596e0eb3cc1dfd9b",
+    "url": "/ps-flux/static/js/main.535257a8.chunk.js"
+  },
+  {
+    "revision": "454f12989080f9d40d73",
+    "url": "/ps-flux/static/js/runtime~main.727e9910.js"
+  }
+]);

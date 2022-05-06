54ed3f9548a153e397b759d6b78c3f61998878e8
jeremie_giffard <e132784f@gmail.com>
2022-05-06 14:13:46 +0200

    notFoundPage module update

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NotFoundPage() {
-  return (
-    <div>
-      <h2>Page Not Found</h2>
-      <p>
-        <Link to="/">Back to Home</Link>
-      </p>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Page Not Found</h2>
+			<p>404 module like, for bogus URL</p>
+			<Link to="/" className="btn btn-primary">
+				Back to Home
+			</Link>
+		</div>
+	);
 }
 
 export default NotFoundPage;
+
